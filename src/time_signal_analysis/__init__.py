da361972cbe88563d8c1b2c21fb7992fdd740462
Goliaten <ognikat4@gmail.com>
2026-01-08 23:25:54 +0100

    remove printout in time analysis import

--- src/time_signal_analysis/__init__.py
+++ src/time_signal_analysis/__init__.py
@@ -1,14 +1,14 @@
 import importlib as il
 import os
-from types import ModuleType
 from typing import Dict
+from src.helpers.CustomTypes import AnalysisModule
 
-analyse_modules: Dict[str, ModuleType] = {}
+analyse_modules: Dict[str, AnalysisModule] = {}
 exclude_entries = ["__init__", "__pycache__"]
 
 for file in os.listdir(__path__[0]):
     file = os.path.splitext(file)[0]
-    print(file)
+
     if file in exclude_entries:
         continue
     module_candidate = il.import_module(f".{file}", __package__)
@@ -16,8 +16,9 @@ for file in os.listdir(__path__[0]):
     try:
         # validade module
         assert module_candidate.__dict__.get("analyse") is not None
+        valid_module: AnalysisModule = module_candidate  # type: ignore
     except AssertionError:
         print(f"Module {file} is invalid. It's missing `analyse` method")
         continue
 
-    analyse_modules[file] = module_candidate
+    analyse_modules[file] = valid_module
