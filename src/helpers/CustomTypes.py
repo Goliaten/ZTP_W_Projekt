65573d30b596fa52477c63a6e2b1f7362d588dc4
Goliaten <ognikat4@gmail.com>
2026-01-08 23:26:07 +0100

    custom module type

---
+++ src/helpers/CustomTypes.py
@@ -0,0 +1,15 @@
+from abc import abstractmethod
+from types import ModuleType
+from typing import Any, Dict
+
+import numpy.typing as npt
+import numpy as np
+
+
+class AnalysisModule(ModuleType):
+    @staticmethod
+    @abstractmethod
+    def analyse(
+        *, data: npt.NDArray[np.integer], freq: int, **kwargs
+    ) -> Dict[str, Any]:
+        pass
