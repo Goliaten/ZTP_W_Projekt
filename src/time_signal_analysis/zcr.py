16861357f5841023c7e688b0e3396fee5c07cb9f
Goliaten <ognikat4@gmail.com>
2026-01-10 00:05:20 +0100

    zcr

--- src/time_signal_analysis/zcr.py
+++ src/time_signal_analysis/zcr.py
@@ -1,3 +1,19 @@
-def analyse():
-    # TODO
-    raise NotImplementedError()
+import numpy as np
+import numpy.typing as npt
+from typing import Dict, Any
+
+
+def analyse(
+    *,
+    data: npt.NDArray[np.integer],
+    raw_data: npt.NDArray[np.integer],
+    freq: int,
+    **kwargs,
+) -> Dict[str, Any]:
+    # TODO verify if this is correct
+
+    zero_crossings = np.where(np.diff(np.sign(data)))[0]
+
+    zcr = len(zero_crossings) / (len(data) / freq)
+
+    return {"zcr": zcr}
