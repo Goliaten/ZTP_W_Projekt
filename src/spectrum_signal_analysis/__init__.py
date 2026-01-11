375e8daccd7de094a2f1a7b82503ee185d270664
Goliaten <ognikat4@gmail.com>
2026-01-11 14:08:43 +0100

    auto importer for spectrum analysis

--- src/spectrum_signal_analysis/__init__.py
+++ src/spectrum_signal_analysis/__init__.py
@@ -1,5 +1,28 @@
-# fourier
-# parzyste
-# nieparzyste
+import importlib as il
+import os
+from typing import Dict
+from src.helpers.CustomTypes import SpectrumModule
 
-# TODO automatyczny import funkcji do odpowiedniej zmiennej
+analyse_modules: Dict[str, SpectrumModule] = {}
+exclude_entries = ["__init__", "__pycache__"]
+
+for file in os.listdir(__path__[0]):
+    file = os.path.splitext(file)[0]
+
+    if file in exclude_entries:
+        continue
+    module_candidate = il.import_module(f".{file}", __package__)
+
+    # validade module
+    try:
+        assert module_candidate.__dict__.get("analyse") is not None
+        # TODO should also check signature of this function
+        valid_module: SpectrumModule = module_candidate  # type: ignore
+    except AssertionError:
+        print(f"Module {file} is invalid. It's missing `analyse` method")
+        continue
+
+    analyse_modules[file] = valid_module
+    del file, valid_module, module_candidate
+
+del os, il, Dict, SpectrumModule
