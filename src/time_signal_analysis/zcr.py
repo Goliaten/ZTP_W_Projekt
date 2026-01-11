66effbf5652d2154b0dbde30b8b32708609676aa
Goliaten <ognikat4@gmail.com>
2026-01-11 14:06:57 +0100

    udpated zcr

--- src/time_signal_analysis/zcr.py
+++ src/time_signal_analysis/zcr.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.typing as npt
 from typing import Dict, Any
 
+from src.helpers.params_helper import Params
+
 
 def analyse(
     *,
@@ -10,7 +12,11 @@ def analyse(
     freq: int,
     **kwargs,
 ) -> Dict[str, Any]:
-    # TODO verify if this is correct
+    main_param = Params.get("time_analysis").get("zcr")
+    num_of_samples = main_param.get("samples_to_analyse")
+
+    max_val = data.max()
+    data = data[max_val : max_val + min(num_of_samples, len(data))]
 
     zero_crossings = np.where(np.diff(np.sign(data)))[0]
 
