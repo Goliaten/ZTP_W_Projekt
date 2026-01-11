ccc662274e8088e58b780f9d99f900edc419c6db
Goliaten <ognikat4@gmail.com>
2026-01-11 14:14:14 +0100

    analyser wip

--- src/analyser.py
+++ src/analyser.py
@@ -1,8 +1,17 @@
+import os
 from typing import Any, Dict
 
+from src.helpers.debug_helper import plot_fft
 from src.helpers.file_helper import open_file
-from src.helpers.signal_cleaner import ommit_noise_in_time_signal
+from src.helpers.params_helper import Params
+from src.helpers.signal_cleaner import (
+    normalise_specter_signal,
+    ommit_noise_in_time_signal,
+)
+from src.helpers.specter_helper import fourier_transform
+from src.helpers.window_helper import windower
 import src.time_signal_analysis as time_analysis
+import src.spectrum_signal_analysis as spectrum_analysis
 
 
 def analyse_soundfile(path_to_file: str) -> Dict[str, Any]:
@@ -10,16 +19,24 @@ def analyse_soundfile(path_to_file: str) -> Dict[str, Any]:
     freq, raw_time_data = open_file(path_to_file)
     # clean time data
     cleaned_time_data = ommit_noise_in_time_signal(raw_time_data, freq)
-    out = {}
+    out: Dict[str, Any] = {"freq": freq}
+
+    # analyse cleaned time data
     for name, mod in time_analysis.analyse_modules.items():
         res = mod.analyse(data=cleaned_time_data, raw_data=raw_time_data, freq=freq)
         out[name] = res
-    from pprint import pprint
 
-    pprint(out)
+    window_len = int(Params.get("window_generator").get("length_sec") * freq)
+    window_overlap = Params.get("window_generator").get("overlap")
+
+    # spectral analysis
+    for name, mod in spectrum_analysis.analyse_modules.items():
+        window_generator = windower(
+            cleaned_time_data, length=window_len, overlap=window_overlap
+        )
+        res = mod.analyse(window_generator=window_generator, freq=freq)
+        out[name] = res
     exit()
-    # analyse cleaned time data
-    # convert time data to spectrum data
     # get window function
     # TODO
     raise NotImplementedError()
